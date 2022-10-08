e387d8268c8507f70d9d006c36332350998f0687
2ashy4u <amoni@ucmerced.edu>
2022-10-08 00:38:09 -0700

    Template Code

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+#initializes Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'westernDigital'
 
     from .views import views
     from .auth import auth
-    
+    #register blueprints for flask
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
+#route to login page
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+#route to logout page
+@auth.route('/manager')
+def logout():
+    return render_template("manager.html")
+@auth.route('/home')
+def home():
+    return render_template("home.html")
 
