a04133ce8873e00026b0afbbd769486f747421a9
2ashy4u <amoni@ucmerced.edu>
2022-10-13 16:58:52 -0700

    commit

--- website/models.py
+++ website/models.py
@@ -22,3 +22,5 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    
+    # additions: manager attribute
