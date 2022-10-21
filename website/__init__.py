b1ac7c80e75a5af6ae7e9fe7846168ff99ed2a80
jdumindin <jndumindin@gmail.com>
2022-10-21 02:46:35 -0700

    Changes to be committed: 	modified:   main.py 	new file:   website/.DS_Store 	modified:   website/__init__.py 	modified:   website/__pycache__/__init__.cpython-310.pyc 	modified:   website/__pycache__/__init__.cpython-39.pyc 	new file:   website/__pycache__/auth.cpython-39.pyc 	new file:   website/__pycache__/models.cpython-39.pyc 	new file:   website/__pycache__/views.cpython-39.pyc 	modified:   website/auth.py 	modified:   website/models.py 	new file:   website/static/.DS_Store 	new file:   website/static/css/main.css 	new file:   website/static/img/.DS_Store 	new file:   website/static/img/westernDigital.png 	new file:   website/static/js/script.js 	new file:   website/templates/.DS_Store 	new file:   website/templates/base.html 	new file:   website/templates/home.html 	new file:   website/templates/landingPage.html 	new file:   website/templates/login.html 	new file:   website/templates/manager.html 	new file:   website/templates/quiz.html 	modified:   website/views.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,46 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+#initializes Database
+db = SQLAlchemy()
+DB_NAME= "users"
+
+#initializes Flask
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'westernDigital'
+    app.config['SECRET_KEY'] = 'Training Monitoring App'
+
+    #below requires existing database called 'userdb'
+    app.config['SQLALCHEMY_DATABASE_URI']=f'mysql://root:@127.0.0.1:3306/{DB_NAME}'
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
-    
+    #register blueprints for flask
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User
+    
+    #populate_db(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
+
+#populate database if not already populated
+#def populate_db(app):
+        #from .models import User
+        #with app.app_context():
+            
+            #new_user = User(email='YAN.LI@wdc.com',password=generate_password_hash('1234', method='sha256'),firstname='Yan',lastname='Li',employeeID='1',isManager='Y',isSupManager='Y')
+                #if not db.session.query(User).filter_by(email='YAN.LI@wdc.com').first():
+                    #db.session.add(new_user)
+                    #db.session.commit()
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,48 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
+#routes to different pages
+@auth.route('/Welcome')
+def land():
+    return render_template("landingPage.html")
+
+@auth.route('/Login', methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        email = request.form.get('email')
+        password=request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password == password:
+                flash('Logged in successfully', category = 'success')
+                login_user(user, remember=True)
+                return redirect(url_for('auth.manager'))
+            else:
+                flash('Wrong password', category = 'error')
+        else:
+            flash('Wrong email', category = 'error')
+    return render_template("login.html", user=current_user)
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/Home')
+def home():
+    return render_template("home.html", user=current_user)
+
+@auth.route('/Manager')
+@login_required
+def manager():
+    return render_template("manager.html", user=current_user)
+
+@auth.route('/Quiz')
+@login_required
+def quiz():
+    return render_template("quiz.html", user=current_user)
 
