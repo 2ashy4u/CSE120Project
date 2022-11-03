594d4222ba4cb3f4cf49b12732ec8b5f45ebec1a
jdumindin <jndumindin@gmail.com>
2022-11-02 17:19:47 -0700

    Add Question Test

--- website/__init__.py
+++ website/__init__.py
@@ -1,47 +1,60 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-#initializes Database
+#import mysql.connector
+# initializes Database
 db = SQLAlchemy()
-DB_NAME= "users"
+DB_NAME = "users"
+# initializes Flask
 
-#initializes Flask
+# init database for cursor
+#conn = mysql.connector.connect(
+#    user='root',
+#    password='',
+#    host='127.0.0.1',
+#    database='users'
+#)
+# inits cursor
+#cursor = conn.cursor()
+
+# checks to see if all tables from database is conected
+#cursor.execute("DESCRIBE userdb.courses")
+# cursor.execute("DESCRIBE user")
+
+# example how to insert data and commit into database ->
+# cursor.execute("INSERT INTO userdb.courses (courseTitle, courseQues) VALUES (%s,%s)",("Course2", "Course Qeustion"))
+# conn.commit()
+
+# for loop to check all data
+# for x in cursor:
+#    print(x)
+
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'westernDigital'
 
-    #below requires existing database called 'userdb'
-    app.config['SQLALCHEMY_DATABASE_URI']=f'mysql://root:@127.0.0.1:3306/{DB_NAME}'
-
+    # below requires existing database called 'userdb' / Connects to local Database using sqlalchemy
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'] = f'mysql://root:@127.0.0.1:3306/{DB_NAME}'
+# initializes database with app
     db.init_app(app)
-
+# more improrts from different files
     from .views import views
     from .auth import auth
-    #register blueprints for flask
+    # register blueprints for flask#register blueprints for flask
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
     from .models import User
-    
-    #populate_db(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    #get user id if needed
-    @login_manager.user_loader
+    # get user id
+    @ login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-    
-    return app
 
-#populate database if not already populated
-#def populate_db(app):
-        #from .models import User
-        #with app.app_context():
-            
-            #new_user = User(email='YAN.LI@wdc.com',password=generate_password_hash('1234', method='sha256'),firstname='Yan',lastname='Li',employeeID='1',isManager='Y',isSupManager='Y')
-                #if not db.session.query(User).filter_by(email='YAN.LI@wdc.com').first():
-                    #db.session.add(new_user)
-                    #db.session.commit()
+    return app
--- website/auth.py
+++ website/auth.py
@@ -1,34 +1,39 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from .models import User, Course
+from . import db
 from flask_login import login_user, logout_user, login_required, current_user
 #from flask_security import roles_required
 
 auth = Blueprint('auth', __name__)
-#routes to different pages
+# routes to different pages
+
+
 @auth.route('/Welcome')
 def land():
     return render_template("landingPage.html")
 
+
 @auth.route('/Login', methods=['GET', 'POST'])
 def login():
-    if request.method=='POST':
+    # recieves GET and POST data
+    if request.method == 'POST':
         email = request.form.get('email')
-        password=request.form.get('password')
-
+        password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
         if user:
             if user.password == password:
-                flash('Logged in successfully', category = 'success')
+                flash('Logged in successfully', category='success')
                 login_user(user, remember=True)
                 if user.isManager == 'N':
                     return redirect(url_for('auth.home'))
                 else:
                     return redirect(url_for('auth.manager'))
             else:
-                flash('Wrong password', category = 'error')
+                flash('Wrong password', category='error')
         else:
-            flash('Wrong email', category = 'error')
+            flash('Wrong email', category='error')
     return render_template("login.html", user=current_user)
+
 
 @auth.route('logout')
 @login_required
@@ -36,19 +41,38 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
+
 @auth.route('/Home')
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    courseLink = request.form.get('courseLink')
+    return render_template("home.html", user=current_user, courseLink=courseLink)
+
 
 @auth.route('/Manager')
 @login_required
-#@roles_required("Manager")
+# @roles_required("Manager")
 def manager():
     return render_template("manager.html", user=current_user)
 
-@auth.route('/Quiz')
-@login_required
-def quiz():
-    return render_template("quiz.html", user=current_user)
 
+@auth.route('/CoursesOverview')
+@login_required
+def coursesOverview():
+    return render_template("coursesOverview.html", user=current_user)
+
+
+@auth.route('/AddCourses', methods=['GET', 'POST'])
+@login_required
+def addCourse():
+    if request.method == 'POST':
+        course = request.form.get('course')
+
+        if len(course) < 1:
+            flash('question too short!', category = 'error')
+        else:
+            new_course = Course(courseQues=course, courseTime='now', user_id=current_user.id)
+            db.session.add(new_course)
+            db.session.commit()
+            flash('Question added.', category = 'success')
+    return render_template("addCourse.html", user=current_user)
