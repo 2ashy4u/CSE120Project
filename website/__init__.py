5c308e3fcaa711edf02cd22416e791d0af818b2b
2ashy4u <amoni@ucmerced.edu>
2022-11-15 02:04:29 -0800

    inserting more info to database
    Needs to automatically add courseid to employeeCourse table

--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 #import mysql.connector
 # initializes Database
 db = SQLAlchemy()
-DB_NAME = "userdb"
+DB_NAME = "users"
 # initializes Flask
 
 # init database for cursor
@@ -36,7 +36,7 @@ def create_app():
 
     # below requires existing database called 'userdb' / Connects to local Database using sqlalchemy
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = f'mysql://root:@127.0.0.1:3306/{DB_NAME}'
+        'SQLALCHEMY_DATABASE_URI'] = f'mysql://root:12345678@127.0.0.1:3306/{DB_NAME}'
 # initializes database with app
     db.init_app(app)
 # more improrts from different files
